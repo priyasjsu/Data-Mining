569cb2de6860d7f6ca03ea4e1ae1eaf4fc0c04a3
priyasjsu <priya.khandelwal@sjsu.edu>
2023-05-18 12:09:30 -0700

    read me

--- setup.py
+++ setup.py
@@ -22,9 +22,10 @@ class ExtractCommand(Command):
         business_restaurant_id = "1V-cj0sZZlvJ4rJCu6mb8bYv4-Xd34vBp"
         cleaned_review_id = "1Vb61c5g1W3QZ4z_77vHeXFFxHfClyht3"
         restaurant_data_id = "1pY6FD1y-9dLhuKJJEDOBlLQmrPjK-SVv"
-
         model_file_id = "1WgnC9NBOeNHwBSauMliSHHRj8nkzP23M"
         business_json = "1-CwSFO8efdwlLHLkA2jtsYJHcJHL3Xw3"
+        mode1_1_id = "14hR-g6u3zFFhBW39-W9fRBKkWUJC2Xt9"
+        mode1_2_id = "1qsm1aX4syg43XXDiDMAPXWw4E6mxQzh3"
 
         file_url = f'https://drive.google.com/uc?id={review_file_id}'
         business_PA_url = f'https://drive.google.com/uc?id={business_PA_id}'
@@ -33,6 +34,8 @@ class ExtractCommand(Command):
         restaurant_data_url = f'https://drive.google.com/uc?id={restaurant_data_id}'
         business_json_url = f'https://drive.google.com/uc?id={business_json}'
         model_url = f'https://drive.google.com/uc?id={model_file_id}'
+        mode1_1_url = f'https://drive.google.com/uc?id={mode1_1_id}'
+        svd_model = f'https://drive.google.com/uc?id={mode1_2_id}'
 
 
         my_path = os.path.abspath(os.path.dirname(__file__))
@@ -42,8 +45,11 @@ class ExtractCommand(Command):
         cleaned_restaurant = os.path.join(my_path, "./data/cleaned_review.csv")
         restaurant_data = os.path.join(my_path, "./data/restaurant_data.csv")
         business_json = os.path.join(my_path, "./data/business.json")
-        model_pickle_review = os.path.join(my_path, "./data/model_pickle_review.pickle")
-        
+        model_file = os.path.join(my_path, "./data/review_model.pickle")
+        model_file_1 = os.path.join(my_path, "./data/train_svd_data.pkl")
+        model_file_2 = os.path.join(my_path, "./data/trained_model.pkl")
+
+
         os.makedirs('./data', exist_ok=True)
         # Retrieve the file content
         gdown.download(file_url, review_csv_PA, quiet=False)
@@ -52,7 +58,10 @@ class ExtractCommand(Command):
         gdown.download(cleaned_review_url, cleaned_restaurant, quiet=False)
         gdown.download(restaurant_data_url, restaurant_data,  quiet=False)
         gdown.download(business_json_url, business_json,  quiet=False)
-        gdown.download(model_url, model_pickle_review,  quiet=False)
+        gdown.download(model_url, model_file,  quiet=False)
+        gdown.download(mode1_1_url, model_file_1,  quiet=False)
+        gdown.download(svd_model, model_file_2,  quiet=False)
+
 
         # zip_path = data_path  # Replace with the actual path to your zip file
         # extract_folder = dest_path  # Replace with the path where you want to extract the files
